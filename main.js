41d566d0c022ce24a1194ddbb3ddf2a9a5a5f7cb
AndoGPP <146361135+AndoGPP@users.noreply.github.com>
2024-02-14 17:57:41 +0100

    the classes and methods done

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
+function Color(red, green, blue, alpha) {
+  this.red = red;
+  this.green = green;
+  this.blue = blue;
+  this.alpha = 1;
+}
+//I only need one function for RGB and RGBa because if not provided, I set it to 1 (full opacity)
+Color.prototype.rgba = function () {
+  return `rgba(${this.red},${this.green}, ${this.blue}, ${this.alpha}`;
+};
 
+Color.prototype.hex = function () {
+const rHex = this.red.toString(16).padStart(2, 0);
+const rHex = this.green.toString(16).padStart(2, 0);
+const rHex = this.blue.toString(16).padStart(2, 0);
+const rHex = this.alpha.Math.round(this.alpha * 255)
+  .toString(16)
+  .padStart(2, 0);
+  return `#(${this.red},${this.green}, ${this.blue}, ${this.alpha}`
+}:
+
+const ColorOne = new Color(123,145,130)
+
+console.log(ColorOne);
